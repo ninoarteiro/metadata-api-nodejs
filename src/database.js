ecf6a6c4abfbaf890bee4aded915ed7d28d44e6c
ninoarteiro <71943253+ninoarteiro@users.noreply.github.com>
2020-10-07 19:33:18 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ module.exports = {
   "1": {
   "description": "xxxxx xxx xxx xxxx", 
   "external_url": "http://ninoarteiro.art/", 
-  "image": "https://cz65qh477d3ex5ywdmfvhhlmgucc6kfh2jvluna4xq3wq6aifsea.arweave.net/Fn3YH5_49kv3FhsLU51sNQQvKKfSaro0HLw3aHgILIg", 
+  "image": "https://gateway.pinata.cloud/ipfs/QmZZmg2JcxTUvCazAwqJ3F6kpMaaGhZjtSyx7NPAwZ7P1W/ADEUS,%20INTERMEDI%C3%81RIOS%21.GIF", 
   "name": "zzzzzzz",
 },
   "2": {
