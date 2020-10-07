38724b2bbd2dff66b4b7ecbdb56ed1ba64dfcaaf
ninoarteiro <71943253+ninoarteiro@users.noreply.github.com>
2020-10-07 19:25:48 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,31 +1,31 @@
 module.exports = {
   "1": {
   "description": "xxxxx xxx xxx xxxx", 
-  "external_url": " http://ninoarteiro.art/", 
+  "external_url": "http://ninoarteiro.art/", 
   "image": "https://cz65qh477d3ex5ywdmfvhhlmgucc6kfh2jvluna4xq3wq6aifsea.arweave.net/Fn3YH5_49kv3FhsLU51sNQQvKKfSaro0HLw3aHgILIg", 
   "name": "zzzzzzz",
 },
   "2": {
   "description": "xxxxx xxx xxx xxxx", 
-  "external_url": " http://ninoarteiro.art/", 
+  "external_url": "http://ninoarteiro.art/", 
   "image": "https://cz65qh477d3ex5ywdmfvhhlmgucc6kfh2jvluna4xq3wq6aifsea.arweave.net/Fn3YH5_49kv3FhsLU51sNQQvKKfSaro0HLw3aHgILIg", 
   "name": "zzzzzzz",
 },
   "3": {
   "description": "xxxxx xxx xxx xxxx", 
-  "external_url": " http://ninoarteiro.art/", 
+  "external_url": "http://ninoarteiro.art/", 
   "image": "https://cz65qh477d3ex5ywdmfvhhlmgucc6kfh2jvluna4xq3wq6aifsea.arweave.net/Fn3YH5_49kv3FhsLU51sNQQvKKfSaro0HLw3aHgILIg", 
   "name": "zzzzzzz",
 },
   "4": {
   "description": "xxxxx xxx xxx xxxx", 
-  "external_url": " http://ninoarteiro.art/", 
+  "external_url": "http://ninoarteiro.art/", 
   "image": "https://cz65qh477d3ex5ywdmfvhhlmgucc6kfh2jvluna4xq3wq6aifsea.arweave.net/Fn3YH5_49kv3FhsLU51sNQQvKKfSaro0HLw3aHgILIg", 
   "name": "zzzzzzz",
 },
   "5": {
   "description": "xxxxx xxx xxx xxxx", 
-  "external_url": " http://ninoarteiro.art/", 
+  "external_url": "http://ninoarteiro.art/", 
   "image": "https://cz65qh477d3ex5ywdmfvhhlmgucc6kfh2jvluna4xq3wq6aifsea.arweave.net/Fn3YH5_49kv3FhsLU51sNQQvKKfSaro0HLw3aHgILIg", 
   "name": "zzzzzzz",
 }
