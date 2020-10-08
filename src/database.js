c05a80d18a743ef6eb7d2c72445177ea6b273f54
ninoarteiro <71943253+ninoarteiro@users.noreply.github.com>
2020-10-08 15:05:25 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,24 +1,18 @@
 module.exports = {
   "1": {
-  "description": "Edition 1/1. Series: TRILOGY OF THE GOODBYE. This art represents the beginning of a new journey in my profession. I got tired of being censored. Artists no longer need intermediaries. All my cryptoarts from now on will be tokenized with my contract and sold only P2P through my own NFT store, which will be completely independent from art tokenization companies. Goodbye, intermediaries!
-
-#Cryptoart #NeoExpressionism #GlitchArt #Autonomy #Freedom #P2P #NFT", 
+  "description": "Edition 1/1. Series: TRILOGY OF THE GOODBYE. This art represents the beginning of a new journey in my profession. I got tired of being censored. Artists no longer need intermediaries. All my cryptoarts from now on will be tokenized with my contract and sold only P2P through my own NFT store, which will be completely independent from art tokenization companies. Goodbye, intermediaries! #Cryptoart #NeoExpressionism #GlitchArt #Autonomy #Freedom #P2P #NFT", 
   "external_url": "http://ninoarteiro.art/", 
   "image": "https://gateway.pinata.cloud/ipfs/QmZZmg2JcxTUvCazAwqJ3F6kpMaaGhZjtSyx7NPAwZ7P1W/ADEUS,%20INTERMEDI%C3%81RIOS%21.GIF", 
   "name": "GOODBYE, INTERMEDIARIES!",
 },
   "2": {
-  "description": "Edition 1/1. Series: EXPRESSIONS.
-
-#Cryptoart #NeoExpressionism #Expressions #Colors #NFT", 
+  "description": "Edition 1/1. Series: EXPRESSIONS. #Cryptoart #NeoExpressionism #Expressions #Colors #NFT", 
   "external_url": "http://ninoarteiro.art/", 
   "image": "https://5qn65inygkcc3r23wedbvd5h2dold3izupsdo5jzqxmt4xc3lu6q.arweave.net/7BvuobgyhC3HW7EGGo-n0Nyx7Rmj5Dd1OYXZPlxbXT0", 
   "name": "EXPRESSION 11",
 },
   "3": {
-  "description": "Edition 1/1. Series: TRILOGY OF THE GOODBYE. The boss (the bourgeois) is a thief!
-
-#Cryptoart #GlitchArt #Neoexpressionism #Collage #Remix #Autonomy #Free! #P2P #NFT", 
+  "description": "Edition 1/1. Series: TRILOGY OF THE GOODBYE. The boss (the bourgeois) is a thief! #Cryptoart #GlitchArt #Neoexpressionism #Collage #Remix #Autonomy #Free! #P2P #NFT", 
   "external_url": "http://ninoarteiro.art/", 
   "image": "https://gateway.pinata.cloud/ipfs/QmZBeDPikK7tdxgmmd8v53xUM37Qjvw3wmQZGWKhz8uFwA/ADEUS,%20PATR%C3%95ES%21.GIF", 
   "name": "GOODBYE, BOSSES!",
