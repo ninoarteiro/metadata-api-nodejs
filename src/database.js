f5b72468194066044747cddd7762a100fca84d8c
ninoarteiro <71943253+ninoarteiro@users.noreply.github.com>
2020-10-15 21:55:06 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -8,25 +8,25 @@ module.exports = {
   "2": {
   "description": "Edition 1/1. Series: EXPRESSIONS. #Cryptoart #NeoExpressionism #Expressions #Colors #NFT", 
   "external_url": "https://beta.cent.co/ninoarteiro", 
-  "image": "https://ipfs.io/ipfs/QmYcPw55wUjybrgMu6Ywk1maeeLr8WMF8zEf5qSySFEx5j?filename=EXPRESS%C3%83O%2011.GIF", 
+  "image": "https://5qn65inygkcc3r23wedbvd5h2dold3izupsdo5jzqxmt4xc3lu6q.arweave.net/7BvuobgyhC3HW7EGGo-n0Nyx7Rmj5Dd1OYXZPlxbXT0", 
   "name": "EXPRESSION 11",
 },
   "3": {
   "description": "Edition 1/1. Series: TRILOGY OF THE GOODBYE. The boss (the bourgeois) is a thief! #Cryptoart #GlitchArt #NeoExpressionism #Collage #Remix #Autonomy #Free! #P2P #NFT", 
   "external_url": "https://beta.cent.co/ninoarteiro", 
-  "image": "https://ipfs.io/ipfs/QmRsnVMp786JnKZ4iwkzReN3e3i4dm1hQdPKvLjE5dCMA6?filename=ADEUS%2C%20PATR%C3%95ES!.GIF", 
+  "image": "https://s6nvj4vcybsjj4r4burxlgmn54mnfkyozsbffkswvsrmnvinwhza.arweave.net/l5tU8qLAZJTyPA0jdZmN7xjSqw7MglKqVqyixtUNsfI", 
   "name": "GOODBYE, BOSSES!",
 },
   "4": {
   "description": "Edition 1/1. Series: TRILOGY OF THE GOODBYE. I discovered a hole in the system that allowed me to say goodbye to censorship. Two images taken from the internet were used to make this remix. #Cryptoart #GlitchArt #NeoExpressionism #Collage #Remix #Autonomy #FreedomOfCreation #P2P #NFT", 
   "external_url": "https://beta.cent.co/ninoarteiro", 
-  "image": "https://ipfs.io/ipfs/QmVShKQ941DsK44RJgA9WFjPXmGPehWALXRJuA8tV5tiT4?filename=ADEUS%2C%20CENSURA!.GIF", 
+  "image": "https://2x5kxe4yom2fxto2lby6y46li5zjfd2kgxig47dn552k3ou5qm3a.arweave.net/1fqrk5hzNFvN2lhx7HPLR3KSj0o10G58be90rbqdgzY", 
   "name": "GOODBYE, CENSORSHIP!",
 },
   "5": {
   "description": "Edition 1/1. Series: EXPRESSIONS. #Cryptoart #NeoExpressionism #Expression #Colors #Remix #P2P #NFT", 
   "external_url": "https://beta.cent.co/ninoarteiro", 
-  "image": "https://ipfs.io/ipfs/QmdJrBAsq87JDDvmSU38ArXDRhE8sNZdjWGpH1A7AfVwTN?filename=EXPRESS%C3%83O%209.GIF", 
+  "image": "https://g4u47fujubi5lotk24qvyedzfwufkldo5j5a7c7lxoufcvuxa2la.arweave.net/NynPlomgUdW6atchXBB5LahVLG7qeg-L67uoUVaXBpY", 
   "name": "EXPRESSION 9",
 },
   "6": {
@@ -38,7 +38,7 @@ module.exports = {
   "7": {
   "description": "Edition 1/1. Series: ART IS NOT UNTOUCHABLE! This remix (a digital punk cubism) was created from the art that started the cubism: “Les Demoiselles d'Avignon” (by Pablo Picasso). #Cryptoart #NeoExpressionism #GlitchArt #DigitalPunkCubism #Remix #Picasso #P2P #NFT", 
   "external_url": "https://beta.cent.co/ninoarteiro", 
-  "image": "https://ipfs.io/ipfs/QmSikSyxK4psLsXMSYG635XMkcYuvRtsnudzFN4gmTjfLC?filename=AS%20SENHORITAS%20DO%20CIBERESPA%C3%87O.GIF", 
+  "image": "https://bc2lnwncekrw6erdzftyxzrkdbkn2wp7mz5cx4skzrupmie3zpha.arweave.net/CLS22aIio28SI8lni-YqGFTdWf9meivySsxo9iCby84", 
   "name": "LES DEMOISELLES DU CYBERSPACE",
 },
   "8": {
